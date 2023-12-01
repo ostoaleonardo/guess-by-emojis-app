e710d545808b5446aea272e3b67be76b3cf0f79a
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-11-30 21:10:08 -0600

    Don't show admob banner if app is offline

--- src/components/BannerAdMob.jsx
+++ src/components/BannerAdMob.jsx
@@ -1,8 +1,13 @@
+import NetInfo from '@react-native-community/netinfo'
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads'
 
 const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-5454307717540089/9669228618'
 
 export function BannerAdMob() {
+    const { isConnected } = NetInfo.useNetInfo()
+    
+    if (!isConnected) return null
+
     return (
         <BannerAd
             unitId={adUnitId}
