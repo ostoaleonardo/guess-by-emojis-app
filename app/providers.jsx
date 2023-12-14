14cf657fb499f85a3f035a67aeb37908a01561b4
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-12-13 20:07:05 -0600

    Remove powerUps context

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,12 +1,9 @@
 import { MoneyProvider } from '../src/context/moneyContext'
-import { PowerUpsProvider } from '../src/context/powerUpsContext'
 
 export function Providers({ children }) {
     return (
         <MoneyProvider>
-            <PowerUpsProvider>
                 {children}
-            </PowerUpsProvider>
         </MoneyProvider>
     )
 }
